730a376f540999bb4e9b7c14736482e9773dc472
Oleg Nykerui <nykeruyoleg96@gmail.com>
2022-08-12 01:01:15 +0300

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+let refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+refs.gallery.addEventListener('click', loadGallery);
+
+refs.gallery.insertAdjacentHTML('beforeend', loadGallery());
+
+function loadGallery() {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        ` <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a> 
+
+`
+    )
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  close: false,
+  showCounter: false,
+});
